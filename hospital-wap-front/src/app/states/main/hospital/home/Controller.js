02c00b00d8516f98593d065c47b5615076123f4b
hahaniu <18341744489@163.com>
2018-01-12 21:24:10 +0800

    1

--- hospital-wap-front/src/app/states/main/hospital/home/Controller.js
+++ hospital-wap-front/src/app/states/main/hospital/home/Controller.js
@@ -43,24 +43,44 @@ class Controller {
         alertService = _alertService;
         wxService = _wxService;
         /////////////////////////////////
-        $scope.getPageInfo=function (page) {
+
+        $scope.firstArr = [];
+        $scope.nextArr = [];
+
+        $scope.getPageInfo=function (page,s) {
             $http({
                 method: 'GET',
                 url: conf.apiPath + '/wap/article/list',
                 params: {
-                    size: conf.pageSize,
-                    page: page,
+                    // size: conf.pageSize,
+                    // page: page,
                 },
             }).then(function (resp) {
                 console.log(resp.data.data.content);
-                $scope.articles=resp.data.data.content;
+                $scope.articles = resp.data.data.content;
+
+                for(let i in $scope.articles ){
+                    if(i <3){
+                        $scope.firstArr.push( $scope.articles[i])
+                    }else{
+                        $scope.nextArr.push( $scope.articles[i])
+                    }
+                }
+
+                console.log(' $scope.firstArr', $scope.firstArr);
+                console.log(' $scope.nextArr', $scope.nextArr);
+
                 // console.log('Success ' + resp.data.data.cdnUrls[0].url);
+                $timeout(function () {
+                    s.update()
+                },50)
+
             }, function (resp) {
                 // console.log('Error status: ' + resp.status);
             });
         };
 
-        $scope.getPageInfo();
+        // $scope.getPageInfo();
     }
 }
 
